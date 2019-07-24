cf0c90ff98258408d3c7d404eeb06263a4934dfc
1eonel <leonelsan@gmail.com>
2019-07-24 15:44:02 -0600

    rfid.py

--- lab3/rfid.py
+++ lab3/rfid.py
@@ -4,14 +4,13 @@ import time
 port = serial.Serial("/dev/ttyACM0", baudrate=9600, timeout=0.2)
 
 
-time.sleep(5)
+time.sleep(1)
 port.write('h')
 
 while True:
 	rcv= port.readline()
 	print( "Tag detected: " , rcv)
+	for c in rcv
+		print(c)
 	
-
-
-
 
